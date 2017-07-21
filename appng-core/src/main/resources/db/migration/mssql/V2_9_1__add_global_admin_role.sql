d5dd10b97c7fbcaccbba510c3ea0294881c54e29
madness-inc <matthias.mueller@appng.org>
2017-07-21 15:16:27 +0200

    APPNG-2019

--- appng-core/src/main/resources/db/migration/mssql/V2_9_1__add_global_admin_role.sql
+++ appng-core/src/main/resources/db/migration/mssql/V2_9_1__add_global_admin_role.sql
@@ -1,3 +1,4 @@
 ALTER TABLE authgroup ADD default_admin bit not null default 0;
+GO
 UPDATE authgroup SET default_admin=1 WHERE name='Administrators';
-INSERT IGNORE INTO authgroup (name,description,default_admin,version) VALUES ('Administrators','appNG Administrators group',1,getdate());
+INSERT INTO authgroup (name,description,default_admin,version) VALUES ('Administrators','appNG Administrators group',1,getdate());
