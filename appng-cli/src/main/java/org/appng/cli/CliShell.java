f721e3708af643a81e898b802615ebc8c0fd2253
madness-inc <matthias.mueller@appng.org>
2017-11-16 14:04:18 +0100

    APPNG-2065 provide a shell for the CLI

---
+++ appng-cli/src/main/java/org/appng/cli/CliShell.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2011-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.appng.cli;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * Wraps the appNG CLI in a shell.
+ * 
+ * @author Matthias Müller
+ *
+ */
+public class CliShell {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("\033[31m");
+
+		ClassLoader classLoader = CliShell.class.getClassLoader();
+		List<String> logo = IOUtils.readLines(classLoader.getResourceAsStream("cli-shell.txt"), StandardCharsets.UTF_8);
+		logo.forEach(l -> System.out.println(l));
+		System.out.println("##########################################################");
+		System.out.print("Loading appNG shell");
+
+		CliBootstrapEnvironment env = new CliBootstrapEnvironment();
+		File platformRootPath = null;
+		String appngHome = System.getProperty(CliBootstrap.APPNG_HOME);
+		if (null != appngHome) {
+			platformRootPath = new File(appngHome).getAbsoluteFile();
+		}
+		if (null == platformRootPath || !platformRootPath.exists()) {
+			platformRootPath = CliBootstrap.getPlatformRootPath(env);
+		}
+
+		Properties cliConfig = CliBootstrap.getCliConfig(env, true, platformRootPath);
+
+		FutureTask<ApplicationContext> futureTask = new FutureTask<ApplicationContext>(
+				new Callable<ApplicationContext>() {
+					public ConfigurableApplicationContext call() throws Exception {
+						return CliBootstrap.getContext(cliConfig, CliBootstrap.CLI_CONTEXT_XML);
+					}
+				});
+
+		ExecutorService executor = Executors.newFixedThreadPool(1);
+		executor.execute(futureTask);
+
+		long start = System.currentTimeMillis();
+		while (!futureTask.isDone()) {
+			System.out.print(".");
+			Thread.sleep(500);
+		}
+		long stop = System.currentTimeMillis();
+		System.out.println("done! duration: " + (stop - start) + "ms");
+		ApplicationContext context = futureTask.get(1, TimeUnit.MINUTES);
+
+		CliCore cliCore = new CliCore();
+		cliCore.setContext(context);
+
+		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+
+		while (true) {
+			System.out.print("appng> ");
+			String commandLine = console.readLine();
+			if ("exit".equals(commandLine.trim())) {
+				System.out.println("bye");
+				System.exit(0);
+			}
+			if (cliCore.processCommand(commandLine.split(StringUtils.SPACE))) {
+				cliCore.perform(cliConfig);
+			}
+		}
+
+	}
+
+}
