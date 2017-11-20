0108885ac9f49896e9d3b3733a8dd50de905aa37
madness-inc <matthias.mueller@appng.org>
2017-11-20 09:16:03 +0100

    APPNG-2068 fixed code examples about testing, added some JavaDoc

---
+++ appng-documentation/src/main/asciidoc/listing/EmployeesTest.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+
+import org.appng.api.FieldProcessor;
+import org.appng.api.ProcessingException;
+import org.appng.api.support.CallableAction;
+import org.appng.api.support.CallableDataSource;
+import org.appng.testsupport.TestBase;
+
+// tag::all[]
+@org.springframework.test.context.ContextConfiguration(locations = {
+        TestBase.TESTCONTEXT_JPA }, initializers = EmployeesTest.class)
+public class EmployeesTest extends TestBase {
+
+    // tag::contructor[]
+    public EmployeesTest() {
+        super("myapp", "application-home");
+        setEntityPackage("com.myapp.domain");
+        setRepositoryBase("com.myapp.repository");
+    }
+    // end::contructor[]
+
+    // tag::datasource[]
+    @org.junit.Test
+    public void testShowEmployees() throws ProcessingException, IOException {
+        addParameter("selectedId", "1"); // <!--1-->
+        initParameters(); // <!--2-->
+        DataSourceCall dataSourceCall = getDataSource("employees");// <!--3-->
+        CallableDataSource callableDataSource = dataSourceCall.getCallableDataSource(); // <!--4-->
+        callableDataSource.perform("aPage"); // <!--5-->
+        validate(callableDataSource.getDatasource());// <!--6-->
+    }
+    // end::datasource[]
+
+    // tag::action[]
+    @org.junit.Test
+    public void testCreateEmployee() throws ProcessingException, IOException {
+        ActionCall action = getAction("employeeEvent", "create");// <!--1-->
+        action.withParam(FORM_ACTION, "create");// <!--2-->
+        Employee formBean = new Employee("John", "Doe");// <!--3-->
+        CallableAction callableAction = action.getCallableAction(formBean);// <!--4-->
+        FieldProcessor fieldProcessor = callableAction.perform();// <!--5-->
+        validate(fieldProcessor.getMessages(), "-messages");// <!--6-->
+        validate(callableAction.getAction(), "-action");// <!--7-->
+    }
+    // end::action[]
+
+}
+// end::all[]
