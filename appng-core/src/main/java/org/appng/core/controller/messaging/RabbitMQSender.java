de17537cc3f03394ef6fa7937951d6132f84f533
madness-inc <matthias.mueller@appng.org>
2018-01-04 17:41:17 +0100

    APPNG-2095 updated amqp-client, drop lyra, re-use connection and channel

--- appng-core/src/main/java/org/appng/core/controller/messaging/RabbitMQSender.java
+++ appng-core/src/main/java/org/appng/core/controller/messaging/RabbitMQSender.java
@@ -17,7 +17,6 @@ package org.appng.core.controller.messaging;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.concurrent.TimeoutException;
 
 import org.appng.api.messaging.Event;
 import org.appng.api.messaging.Sender;
@@ -25,51 +24,42 @@ import org.appng.api.messaging.Serializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-
-import net.jodah.lyra.ConnectionOptions;
-
 /**
  * Message sender implementing {@link Sender} to use a RabbitMQ message broker. See {@link RabbitMQReceiver} for
  * configuration details.
  * 
  * @author Claus Stümke, aiticon GmbH, 2015
+ * @author Matthias Müller
  * @see RabbitMQReceiver
- *
  */
 public class RabbitMQSender extends RabbitMQBase implements Sender {
 
 	private static final String APPNG_MESSAGING_DISABLED = "appng.messaging.disabled";
 	private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQSender.class);
 
-	@Override
 	public RabbitMQSender configure(Serializer eventDeserializer) {
 		this.eventSerializer = eventDeserializer;
-		initialize();
+		initialize("appng-rabbitmq-sender-%d");
 		return this;
 	}
 
-	@Override
 	public boolean send(Event event) {
 		if (!Boolean.getBoolean(APPNG_MESSAGING_DISABLED)) {
-			Connection connection = null;
-			Channel channel = null;
 			try {
-				connection = getConnection(new ConnectionOptions(factory));
-				channel = connection.createChannel();
 				ByteArrayOutputStream out = new ByteArrayOutputStream();
 				eventSerializer.serialize(out, event);
 				LOGGER.info("sending {} to rabbitMQ host(s) {} exchange {}", event, this.addresses, this.exchange);
-				channel.exchangeDeclare(this.exchange, EXCHANGE_TYPE_FANOUT);
 				channel.basicPublish(this.exchange, "", null, out.toByteArray());
-				channel.close();
-				connection.close();
 				return true;
-			} catch (IOException | TimeoutException e) {
+			} catch (IOException e) {
 				LOGGER.error("error while sending event " + event, e);
 			}
 		}
 		return false;
 	}
+
+	Logger log() {
+		return LOGGER;
+	}
+
 }
