6acb0f6f155d753da23df48c7396fd3ff02e1e89
madness-inc <matthias.mueller@appng.org>
2018-01-11 12:30:59 +0100

    APPNG-2105 fixed missing :

--- appng-core/src/main/java/org/appng/core/controller/handler/SoapService.java
+++ appng-core/src/main/java/org/appng/core/controller/handler/SoapService.java
@@ -227,7 +227,7 @@ public class SoapService {
 		String servicePath = site.getProperties().getString(SiteProperties.SERVICE_PATH);
 		String domain = site.getDomain();
 		if (!domain.startsWith(HTTPS) && HttpHeaders.isRequestSecure(request)) {
-			domain = HTTPS + domain.substring(5, domain.length());
+			domain = HTTPS + domain.substring(4, domain.length());
 		}
 		return domain + servicePath + SLASH + site.getName() + SLASH + application.getName() + SLASH
 				+ Platform.SERVICE_TYPE_SOAP + SLASH + serviceName;
