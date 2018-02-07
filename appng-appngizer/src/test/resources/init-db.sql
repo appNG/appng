516d2ba2116d6ed30d98cd0c393aa83175ed20fe
madness-inc <matthias.mueller@appng.org>
2018-02-07 10:15:07 +0100

    APPNG-2112 support managed-attribute for root connection

--- appng-appngizer/src/test/resources/init-db.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO database_connection
-(description,driver_class,jdbc_url,name,password,type,username,site_id,min_connections,max_connections,managed,active,validation_query,version)
-VALUES ('appNG Root Database','org.hsqldb.jdbc.JDBCDriver','jdbc:hsqldb:mem:testdb','appNG HSQL','','HSQL','sa',null,1,20,false,false,'select 1 from INFORMATION_SCHEMA.SYSTEM_USERS',now());
