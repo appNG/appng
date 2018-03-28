51c5cc4629141558201646ddf1a6ac2fa3978f30
madness-inc <matthias.mueller@appng.org>
2018-03-28 11:01:52 +0200

    APPNG-2150 support UTF-8, read name and version from pom.xml

--- appng-xmlapi/src/test/resources/ApplicationProperty.java
+++ appng-xmlapi/src/test/resources/ApplicationProperty.java
@@ -9,5 +9,7 @@ public class ApplicationProperty {
 	public static final String PROP_PROPERTY_B = "propertyB";
 	/** this is property c (multilined) */
 	public static final String PROP_PROPERTY_C = "propertyC";
+	/** contains a dot and a UTF-8 character */
+	public static final String PROP_SOME_UTF_8_€ = "some.utf-8-€";
 
 }
