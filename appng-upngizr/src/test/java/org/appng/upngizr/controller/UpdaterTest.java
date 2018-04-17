b61277489acdba9ad8a40cab44f4b30acbbbdb7b
madness-inc <matthias.mueller@appng.org>
2018-04-17 15:12:34 +0200

    APPNG-2170 write WEB-INF/classes for appNGizer

--- appng-upngizr/src/test/java/org/appng/upngizr/controller/UpdaterTest.java
+++ appng-upngizr/src/test/java/org/appng/upngizr/controller/UpdaterTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.appng.upngizr.controller;
 
 import java.io.File;
@@ -17,13 +32,7 @@ public class UpdaterTest {
 	@Test
 	public void testUpdateAppNG() throws Exception {
 		target = new File("target/appNG");
-		File[] files = new File("../appng-application/target").listFiles(new FilenameFilter() {
-			public boolean accept(File dir, String name) {
-				return name.startsWith("appng-application-") && name.endsWith(".war");
-			}
-		});
-		Assume.assumeTrue(files.length == 1);
-		Resource resource = new FileSystemResource(files[0]);
+		Resource resource = getResource("appng-application");
 		new Updater(new MockServletContext()).updateAppNG(resource, target.getAbsolutePath());
 		assertFolderNotEmpty("WEB-INF");
 		assertFolderNotEmpty("WEB-INF/classes");
@@ -35,18 +44,22 @@ public class UpdaterTest {
 	public void testUpdateAppNGizer() throws Exception {
 		target = new File("target/appNGizer");
 		target.mkdirs();
-		File[] files = new File("../appng-appngizer/target").listFiles(new FilenameFilter() {
-			public boolean accept(File dir, String name) {
-				return name.startsWith("appng-appngizer-") && name.endsWith(".war");
-			}
-		});
-		Assume.assumeTrue(files.length == 1);
-		Resource resource = new FileSystemResource(files[0]);
+		Resource resource = getResource("appng-appngizer");
 		new Updater(new MockServletContext()).updateAppNGizer(resource, target.getAbsolutePath());
 		assertFolderNotEmpty("WEB-INF");
 		assertFolderNotEmpty("WEB-INF/classes/org/appng/appngizer/model/");
 		assertFolderNotEmpty("WEB-INF/classes/org/appng/appngizer/controller/");
 		assertFolderNotEmpty("WEB-INF/lib");
+	}
+
+	protected Resource getResource(String artifactName) {
+		File[] files = new File("../" + artifactName + "/target").listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.startsWith(artifactName) && name.endsWith(".war");
+			}
+		});
+		Assume.assumeTrue(files.length == 1);
+		return new FileSystemResource(files[0]);
 	}
 
 	private void assertFolderNotEmpty(String path) {
