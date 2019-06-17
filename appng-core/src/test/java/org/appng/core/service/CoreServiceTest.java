31e4b8ca5c33cd8153e7fdfef270be3bc95cf5e2
madness-inc <matthias.mueller@appng.org>
2019-06-17 15:42:16 +0200

    APPNG-2269 fix test

--- appng-core/src/test/java/org/appng/core/service/CoreServiceTest.java
+++ appng-core/src/test/java/org/appng/core/service/CoreServiceTest.java
@@ -388,7 +388,7 @@ public class CoreServiceTest {
 		assertFalse(fp.hasErrors());
 	}
 
-	@Test(timeout = 2000)
+	@Test(timeout = 20000)
 	public void testDeleteSiteWithEnvironment() throws BusinessException, IOException, InterruptedException {
 		SiteImpl site = coreService.getSite(2);
 		Map<String, Site> siteMap = environment.getAttribute(Scope.PLATFORM, Platform.Environment.SITES);
@@ -411,6 +411,7 @@ public class CoreServiceTest {
 		while (null == nodeStates.get(nodeId)) {
 			Thread.sleep(100);
 		}
+		CacheService.createCacheManager(new java.util.Properties());
 		coreService.deleteSite(environment, site);
 		// 5x SiteStateEvent(STARTING, STARTED, STOPPING, STOPPED, DELETED)
 		// 5x NodeEvent
