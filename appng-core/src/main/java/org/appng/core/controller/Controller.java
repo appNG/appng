b779e059b8bcbaf27262095e516d83ed7a51e43f
madness-inc <matthias.mueller@appng.org>
2019-10-15 12:21:13 +0200

    APPNG-2269 imports

--- appng-core/src/main/java/org/appng/core/controller/Controller.java
+++ appng-core/src/main/java/org/appng/core/controller/Controller.java
@@ -21,7 +21,6 @@ import static org.appng.api.Scope.SESSION;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import javax.servlet.Servlet;
