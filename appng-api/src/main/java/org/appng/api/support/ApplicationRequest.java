5806b214ba239960b26f90177ac6fb545a49a35c
Matthias Müller <matthias.mueller@appng.org>
2019-12-16 15:38:58 +0100

    APPNG-2208 introduced PATH variable for include conditions (#115)

--- appng-api/src/main/java/org/appng/api/support/ApplicationRequest.java
+++ appng-api/src/main/java/org/appng/api/support/ApplicationRequest.java
@@ -20,6 +20,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -31,11 +33,14 @@ import org.appng.api.FieldConverter;
 import org.appng.api.FieldProcessor;
 import org.appng.api.MessageParam;
 import org.appng.api.ParameterSupport;
+import org.appng.api.Path;
 import org.appng.api.PermissionProcessor;
 import org.appng.api.Request;
 import org.appng.api.RequestSupport;
+import org.appng.api.Scope;
 import org.appng.api.ValidationProvider;
 import org.appng.api.model.Subject;
+import org.appng.api.support.environment.EnvironmentKeys;
 import org.appng.el.ExpressionEvaluator;
 import org.appng.forms.FormUpload;
 import org.appng.forms.RequestContainer;
@@ -48,11 +53,9 @@ import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.http.HttpHeaders;
 
 /**
- * 
  * Default {@link Request} implementation, mostly delegating method-calls to one of the internal objects.
  * 
  * @author Matthias Müller
- * 
  */
 public class ApplicationRequest implements Request {
 
@@ -359,4 +362,96 @@ public class ApplicationRequest implements Request {
 		return headers;
 	}
 
+	/**
+	 * Creates a new {@link ApplicationPath} to be used to evaluate include-conditions on a page.
+	 * 
+	 * @return the {@link ApplicationPath}
+	 * @see    Path#getApplicationUrlParameters()
+	 * @since 1.21
+	 */
+	public ApplicationPath applicationPath() {
+		Path path = getEnvironment().getAttribute(Scope.REQUEST, EnvironmentKeys.PATH_INFO);
+		Map<String, String> parameters = getParameters();
+		List<String> urlParameterNames = path.getApplicationUrlParameters();
+		Map<String, String> urlParameters = urlParameterNames.stream()
+				.collect(Collectors.toMap(Function.identity(), u -> parameters.get(u)));
+		List<String> urlParameterValues = urlParameterNames.stream().map(u -> urlParameters.get(u))
+				.collect(Collectors.toList());
+		return new ApplicationPath('/' + StringUtils.join(urlParameterValues, '/'), urlParameters);
+	}
+
+	/**
+	 * Helper class used to simplify include conditions for actions and datasources on a page.<br/>
+	 * Registered as variable {@code PATH}, so conditions can look like
+	 * 
+	 * <pre>
+	 * &lt;condition expression="${ PATH.starts('/create') }"/>
+	 * </pre>
+	 * 
+	 * or
+	 * 
+	 * <pre>
+	 * &lt;condition expression="${ PATH.starts('/update/', itemId) }"/>
+	 * </pre>
+	 * 
+	 * All methods that can be used:
+	 * <ul>
+	 * <li>{@code matches(String... elements)}
+	 * <li>{@code starts(String... elements)}
+	 * <li>{@code ends(String... elements)}
+	 * <li>{@code contains(String... elements)}
+	 * <li>{@code equals(String... elements)}
+	 * <li>{@code hasParam(String... params)}
+	 * </ul>
+	 * 
+	 * @since 1.21
+	 */
+	public static class ApplicationPath {
+		public static final String PATH = "PATH";
+		final String path;
+		final Map<String, String> variables;
+
+		ApplicationPath(String path, Map<String, String> variables) {
+			this.path = path;
+			this.variables = variables;
+		}
+
+		public boolean hasParam(String... variables) {
+			for (String v : variables) {
+				if (!this.variables.containsKey(v)) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public boolean matches(String... elements) {
+			return path.matches(getExpected(elements));
+		}
+
+		public boolean starts(String... elements) {
+			return path.startsWith(getExpected(elements));
+		}
+
+		public boolean ends(String... elements) {
+			return path.endsWith(getExpected(elements));
+		}
+
+		public boolean contains(String... elements) {
+			return path.contains(getExpected(elements));
+		}
+
+		public boolean equals(String... elements) {
+			return path.equals(getExpected(elements));
+		}
+
+		private String getExpected(String... elements) {
+			return StringUtils.join(elements);
+		}
+
+		@Override
+		public String toString() {
+			return path + " (" + StringUtils.join(variables, ",") + ")";
+		}
+	}
 }
