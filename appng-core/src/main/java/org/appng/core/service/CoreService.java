7126611a04679f329ce184c6a1b6c3ac672241da
madness-inc <matthias.mueller@appng.org>
2019-12-19 10:46:43 +0100

    revert accidentally merged changes

--- appng-core/src/main/java/org/appng/core/service/CoreService.java
+++ appng-core/src/main/java/org/appng/core/service/CoreService.java
@@ -25,7 +25,6 @@ import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
