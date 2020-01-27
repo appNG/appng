d0e58cf23976e9d939513080c1001393ac9db460
madness-inc <matthias.mueller@appng.org>
2020-01-27 11:14:50 +0100

    APPNG-2319 improved handling of user accounts

--- appng-core/src/main/resources/db/migration/hsql/V4_2__add_subject_columns.sql
+++ appng-core/src/main/resources/db/migration/hsql/V4_2__add_subject_columns.sql
@@ -1,6 +1,5 @@
-alter table subject
-add last_login timestamp,
-add pw_last_changed timestamp,
-add locked_since timestamp,
-add login_attempts integer not null default 0,
-add allow_change_pw boolean not null default 1;
+alter table subject add column last_login timestamp;
+alter table subject add column pw_last_changed timestamp;
+alter table subject add column locked_since timestamp;
+alter table subject add column login_attempts integer default 0 not null;
+alter table subject add column allow_change_pw boolean default 1 not null;
