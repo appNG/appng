44e258a43b5341f886e516afa81396ab556ebea4
Julian Kahl <julian.kahl@aiticon.com>
2021-06-08 16:18:01 +0200

    APPNG-2380 handle unchecked checkbox fields

---
+++ appng-api/src/main/java/org/appng/api/support/field/CheckboxFieldConverter.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2011-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.appng.api.support.field;
+
+import org.appng.api.Environment;
+import org.appng.api.FieldWrapper;
+import org.appng.forms.RequestContainer;
+import org.slf4j.Logger;
+import org.springframework.context.MessageSource;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * A {@link FieldConverter} for {@link FieldDef}initions of type {@link FieldType#CHECKBOX}
+ * 
+ * @author Julian Kahl
+ */
+@Slf4j
+public class CheckboxFieldConverter extends ConverterBase {
+
+	CheckboxFieldConverter(Environment environment, MessageSource messageSource) {
+		this.environment = environment;
+		this.messageSource = messageSource;
+	}
+
+	@Override
+	public void setObject(FieldWrapper field, RequestContainer request) {
+		Boolean value = Boolean.valueOf(request.hasParameter(field.getBinding()));
+		field.setObject(value);
+		logSetObject(field, value);
+	}
+
+	@Override
+	public void setString(FieldWrapper field) {
+		Object object = field.getObject();
+		if (null != object) {
+			field.setStringValue(object.toString());
+		}
+	}
+
+	@Override
+	protected Logger getLog() {
+		return LOGGER;
+	}
+}
