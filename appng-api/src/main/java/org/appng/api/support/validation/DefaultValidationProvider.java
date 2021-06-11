f0dc24551f88d68becf7a385d297e7dd2f5d72cd
madness-inc <matthias.mueller@appng.org>
2021-06-11 13:34:18 +0200

    APPNG-2384

--- appng-api/src/main/java/org/appng/api/support/validation/DefaultValidationProvider.java
+++ appng-api/src/main/java/org/appng/api/support/validation/DefaultValidationProvider.java
@@ -27,6 +27,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
 import java.util.Set;
 
 import javax.validation.Configuration;
@@ -549,7 +550,7 @@ public class DefaultValidationProvider implements ValidationProvider {
 	}
 
 	private void sortFieldMessages(FieldDef field) {
-		field.getMessages().getMessageList().sort(MESSAGE_SORTER);
+		Optional.ofNullable(field.getMessages()).orElse(new Messages()).getMessageList().sort(MESSAGE_SORTER);
 	}
 
 	private Message addFieldMessage(FieldDef field, String reference, ConstraintViolation<?> cv) {
