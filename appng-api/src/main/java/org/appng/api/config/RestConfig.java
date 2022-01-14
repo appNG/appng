52affba349845ed9aa10cba8763c6618d9c93a58
madness-inc <matthias.mueller@appng.org>
2022-01-14 15:12:41 +0100

    APPNG-2396 provide a default mapper and converter

--- appng-api/src/main/java/org/appng/api/config/RestConfig.java
+++ appng-api/src/main/java/org/appng/api/config/RestConfig.java
@@ -1,5 +1,23 @@
+/*
+ * Copyright 2011-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.appng.api.config;
 
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,7 +30,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -27,10 +44,25 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 import org.springframework.web.util.UrlPathHelper;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
 import lombok.AllArgsConstructor;
 
+/**
+ * A {@link Configuration} providing commonly used {@link HttpMessageConverter}s (for binary and JSON content) and a
+ * {@link HandlerMethodArgumentResolver} that can resolve
+ * 
+ * @author Matthias Müller
+ */
 @Configuration
 public class RestConfig {
 
@@ -70,16 +102,46 @@ public class RestConfig {
 		return new ByteArrayHttpMessageConverter();
 	}
 
+	/**
+	 * Creates an {@link ObjectMapper} that can (de)serialize {@link OffsetDateTime} using the
+	 * {@link DateTimeFormatter#ISO_DATE_TIME} pattern.
+	 * 
+	 * @return the {@link ObjectMapper}
+	 */
 	@Bean
-	@Primary
-	public MappingJackson2HttpMessageConverter defaultJsonConverter(
+	public ObjectMapper defaultObjectMapper() {
+		SimpleModule dateModule = new SimpleModule();
+		dateModule.addSerializer(OffsetDateTime.class, new JsonSerializer<OffsetDateTime>() {
+			@Override
+			public void serialize(OffsetDateTime value, JsonGenerator jsonGenerator, SerializerProvider provider)
+					throws IOException {
+				if (value != null) {
+					jsonGenerator.writeString(DateTimeFormatter.ISO_DATE_TIME.format(value));
+				}
+			}
+		});
+		dateModule.addDeserializer(OffsetDateTime.class, new JsonDeserializer<OffsetDateTime>() {
+			@Override
+			public OffsetDateTime deserialize(JsonParser parser, DeserializationContext ctxt)
+					throws IOException, JacksonException {
+				return OffsetDateTime.parse(parser.getText(), DateTimeFormatter.ISO_DATE_TIME);
+			}
+		});
+		// @formatter:off
+		return new ObjectMapper()
+			.setDefaultPropertyInclusion(Include.NON_ABSENT)
+			.enable(SerializationFeature.INDENT_OUTPUT)
+			.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+			.registerModule(dateModule);
+		// @formatter:on
+	}
+
+	@Bean
+	public MappingJackson2HttpMessageConverter defaultJsonConverter(ObjectMapper objectMapper,
 			@Value("${site.jsonPrettyPrint:false}") boolean prettyPrint) {
-		ObjectMapper objectMapper = new ObjectMapper();
-		objectMapper.setDefaultPropertyInclusion(Include.NON_ABSENT);
-		MappingJackson2HttpMessageConverter defaultJsonConverter = new MappingJackson2HttpMessageConverter(
-				objectMapper);
-		defaultJsonConverter.setPrettyPrint(prettyPrint);
-		return defaultJsonConverter;
+		MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter(objectMapper);
+		jsonConverter.setPrettyPrint(prettyPrint);
+		return jsonConverter;
 	}
 
 	@Bean
@@ -93,6 +155,10 @@ public class RestConfig {
 		return new ArrayList<>(context.getBeansOfType(HandlerMethodArgumentResolver.class).values());
 	}
 
+	/**
+	 * A {@link HandlerMethodArgumentResolver} that can resolve the current {@link Application}, {@link Environment} and
+	 * {@link Site}.
+	 */
 	@AllArgsConstructor
 	public static class SiteAwareHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
 
