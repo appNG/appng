afd00c699f84dd7c77c34a332196f56c69a4104e
Matthias Müller <Matthias.Mueller@aiticon.com>
2022-05-19 15:39:09 +0200

    APPNG-2440

--- appng-application/src/main/webapp/WEB-INF/bin/appng-shell.bat
+++ appng-application/src/main/webapp/WEB-INF/bin/appng-shell.bat
@@ -1,9 +1,9 @@
 @echo off
 
 REM Directory where the Tomcat binary distribution resides
-SET CATALINA_HOME=D:\servers\apache-tomcat-7.0.67
+SET CATALINA_HOME=D:\servers\apache-tomcat-9.0.63
 
 REM Root directory of the appNG web application
 SET APPNG_HOME=%~dp0\..\..
 
-java -cp "%APPNG_HOME%"/WEB-INF/lib/*;"%CATALINA_HOME%"/lib/* -Dlog4j.configuration=file:"%APPNG_HOME%"/WEB-INF/conf/log4j-cli.properties -Dappng.home="%APPNG_HOME%" org.appng.cli.CliShell
+java -cp "%APPNG_HOME%"/WEB-INF/lib/*;"%CATALINA_HOME%"/lib/* -Dlog4j2.configuration=file:"%APPNG_HOME%"/WEB-INF/conf/log4j2-cli.xml org.appng.cli.CliShell
