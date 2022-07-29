32f640f697ce8c43eaa97bbd76663d807dbe027d
Dirk Heuvels <Dirk.Heuvels@aiticon.com>
2022-07-29 12:28:05 +0200

    APPNG-2442 Removed unused import.

--- appng-core/src/main/java/org/appng/core/controller/handler/RestService.java
+++ appng-core/src/main/java/org/appng/core/controller/handler/RestService.java
@@ -20,7 +20,6 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
