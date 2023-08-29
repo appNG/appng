f8a532b44681a0753e50d43da3e16cc3c2e324aa
Dirk Heuvels <dirk.heuvels@aiticon.com>
2023-08-29 11:41:15 +0200

    INFRA-5668 Replace obsolete appng-standalone (#245)

--- appng-standalone/build.sh
+++
@@ -1,7 +0,0 @@
-export VERSION=0.18.1-SNAPSHOT
-rm -rf appng-standalone-$VERSION/
-mvn clean package
-cd target
-unzip appng-standalone-$VERSION.zip
-cd appng-standalone-$VERSION
-java -Dappng.node.id=appNGizer -jar appng-standalone-$VERSION.jar -p 8080
